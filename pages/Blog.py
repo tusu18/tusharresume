741e10322f72b9e071925ef64e9a58f42f155189
Tushar Singh <42227745+tusu18@users.noreply.github.com>
2023-11-26 06:07:29 +0000

    pages

--- pages/Blog.py
+++ pages/Blog.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 import time
 #from streamlit_option_menu import option_menu
-#from streamlit_extras.switch_page_button import switch_page
+from streamlit_extras.switch_page_button import switch_page
 curr_dir=Path(__file__).parent if "__file__" in locals() else Path.cwd()
 cssfile=curr_dir / "main.css"
 TITLE= "Blogs"
@@ -20,8 +20,14 @@ button_text = "Home", "MyNotes", "Blogs", "Data"
 
 for text, col in zip(button_text, st.columns(len(button_text))):
     if col.button(text):
-        st.write("#")               
-st.subheader("Blogs")
+        if text=="Home":
+            switch_page("app")
+        elif text == "Blogs":
+            switch_page("Blog")
+        elif text == "MyNotes":
+            switch_page("Notes")
+        elif text == "Data":
+            switch_page("Data")  
 st.write("""
 
     One of the most common applications of generative AI and large language models (LLMs) is answering questions based on a specific external knowledge corpus. Retrieval-Augmented Generation (RAG) is a popular technique for building question answering systems that use an external knowledge base. To learn more, refer to Build a powerful question answering bot with Amazon SageMaker, Amazon OpenSearch Service, Streamlit, and LangChain.
